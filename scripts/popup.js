d1c2202e4226c2055fade91373680bb0bf968b75
Karin Cristina Daniel <kacrisdani@hotmail.com>
2024-10-10 18:56:37 -0300

    revisado

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,59 +1,56 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const editButton = document.querySelector('.profile__edit_button');
-    const popupContainer = document.querySelector('.popup__container');
-    const closeButton = document.querySelector('.popup__close_button');
+const editButton = document.querySelector('.profile__edit_button');
+const popupContainer = document.querySelector('.popup__container');
+const closeButton = document.querySelector('.popup__close_button');
 
-    editButton.addEventListener('click', () => {
-        popupContainer.classList.add('active');  
-    });
-
-    closeButton.addEventListener('click', () => {
-        popupContainer.classList.remove('active'); 
-    });
-
-    popupContainer.addEventListener('click', (event) => {
-        if (event.target === popupContainer) {
-            popupContainer.classList.remove('active');
-        }
-    });
-
-
-    let formElement = document.querySelector('.popup__form');
-
-    function handleProfileFormSubmit(evt) {
-        evt.preventDefault();
-
-        const nameInput = document.querySelector('#nome');
-        const jobInput = document.querySelector('#profissao');
-
-        const nome = nameInput.value;
-        const profissao = jobInput.value;
-
-        const profileName = document.querySelector('.profile__name');
-        const profileJob = document.querySelector('.profile__job');
-
-        profileName.textContent = nome;
-        profileJob.textContent = profissao;
-
-        popupContainer.classList.remove('active')
-
-    }
-    
-    formElement.addEventListener('submit', handleProfileFormSubmit);
-
-    const likeButtons = document.querySelectorAll('.gallery__buttonLike');
-    
-    likeButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            const likeIcon = button.querySelector('img');
-            button.classList.toggle('active');
-        
-        if (button.classList.contains('active')) {
-            likeIcon.src = './images/likeactive.svg';
-        } else {
-            likeIcon.src = './images/like.svg';
-        }
-    });
+editButton.addEventListener('click', () => {
+    popupContainer.classList.add('active');  
 });
+
+closeButton.addEventListener('click', () => {
+    popupContainer.classList.remove('active'); 
+});
+
+popupContainer.addEventListener('click', (event) => {
+    if (event.target === popupContainer) {
+        popupContainer.classList.remove('active');
+    }
+});
+
+
+const formElement = document.querySelector('.popup__form');
+
+function handleProfileFormSubmit(evt) {
+    evt.preventDefault();
+
+    const nameInput = document.querySelector('#nome');
+    const jobInput = document.querySelector('#profissao');
+
+    const nome = nameInput.value;
+    const profissao = jobInput.value;
+
+    const profileName = document.querySelector('.profile__name');
+    const profileJob = document.querySelector('.profile__job');
+
+    profileName.textContent = nome;
+    profileJob.textContent = profissao;
+
+    popupContainer.classList.remove('active')
+
+}
+
+formElement.addEventListener('submit', handleProfileFormSubmit);
+
+const likeButtons = document.querySelectorAll('.gallery__buttonLike');
+
+likeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const likeIcon = button.querySelector('img');
+        button.classList.toggle('active');
     
+    if (button.classList.contains('active')) {
+        likeIcon.src = './images/likeactive.svg';
+    } else {
+        likeIcon.src = './images/like.svg';
+    }
+});
 });
